e59601b3cb80d31ff9384c9b1168ea57defe1c81
Miguel de Icaza <miguel@gnome.org>
2010-11-25 14:39:37 -0500

    Add an NSDocument sample

---
+++ DocumentSample/AppDelegate.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Drawing;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+using MonoMac.ObjCRuntime;
+
+namespace DocumentSample
+{
+	public partial class AppDelegate : NSApplicationDelegate
+	{
+		public AppDelegate ()
+		{
+		}
+	}
+}
+
---
+++ DocumentSample/MyDocument.cs
@@ -0,0 +1,42 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+namespace DocumentSample
+{
+	[Register ("MyDocument")]
+	public partial class MyDocument : NSDocument
+	{
+		// Called when created from unmanaged code
+		public MyDocument (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+
+		// Called when created directly from a XIB file
+		[Export("initWithCoder:")]
+		public MyDocument (NSCoder coder) : base(coder)
+		{
+			Initialize ();
+		}
+
+		void Initialize ()
+		{
+		}
+		
+		// Override returning the nib file name of the document
+		// If you need to use a subclass of NSWindowController or if your document supports
+		// multiple NSWindowControllers remove this method and override MakeWindowControllers
+		// instead		
+		public override string WindowNibName {
+			get {
+
+				return "MyDocument";
+			}
+		}
+	}
+}
+
