5186820a1406818fb4cb5b63409688d5f3a0703d
Miguel de Icaza <miguel@gnome.org>
2011-01-18 15:01:29 -0500

    C# 4.0 ify the sample

--- QCBackground/SharedContentView.cs
+++ QCBackground/SharedContentView.cs
@@ -17,32 +17,10 @@ namespace QCBackground
 		
 		NSImageView mover;
 		
-		#region Constructors
-
-		// Called when created from unmanaged code
-		public SharedContentView (IntPtr handle) : base(handle)
-		{
-			Initialize ();
-		}
-
-		// Called when created directly from a XIB file
-		[Export("initWithCoder:")]
-		public SharedContentView (NSCoder coder) : base(coder)
-		{
-			Initialize ();
-		}
-
-		// Shared initialization code
-		void Initialize ()
-		{
-		}
-		
-		#endregion
-		
 		public override void AwakeFromNib ()
 		{
 			WantsLayer = true;
-			Layer = makeCompositionLayer();
+			Layer = MakeCompositionLayer();
 		}
 		
 		public override bool AcceptsFirstResponder ()
@@ -52,78 +30,78 @@ namespace QCBackground
 		
 		public override void KeyDown (NSEvent theEvent)
 		{
-			if ((theEvent.ModifierFlags & NSEventModifierMask.AlphaShiftKeyMask) == NSEventModifierMask.AlphaShiftKeyMask
-			     || (theEvent.ModifierFlags & NSEventModifierMask.ShiftKeyMask) == NSEventModifierMask.ShiftKeyMask)
-			{
-				NSAnimationContext.BeginGrouping();
+			if ((theEvent.ModifierFlags & NSEventModifierMask.AlphaShiftKeyMask) == NSEventModifierMask.AlphaShiftKeyMask ||
+			    (theEvent.ModifierFlags & NSEventModifierMask.ShiftKeyMask) == NSEventModifierMask.ShiftKeyMask){
+				NSAnimationContext.BeginGrouping ();
 				NSAnimationContext.CurrentContext.Duration = 2.0f;
 			}
 			
-			if (theEvent.Characters.ToUpper()[0] == 'A') 
-			{
-				NSView view = Mover();
-				if (view.Superview == null)
-				{
+			if (theEvent.Characters.ToUpper ()[0] == 'A') {
+				NSView view = Mover ();
+				if (view.Superview == null){
 					// there is a problem here with AddSubview with animator
 					//((NSView)Animator).AddSubview(view);
 					this.AddSubview(view);
-				}
-				else
-				{
+				} else 
 					((NSView)view.Animator).RemoveFromSuperview();
-				}
-			}
-			else
+			} else 
 				base.KeyDown(theEvent);
 			
-			if ((theEvent.ModifierFlags & NSEventModifierMask.AlphaShiftKeyMask) == NSEventModifierMask.AlphaShiftKeyMask
-			     || (theEvent.ModifierFlags & NSEventModifierMask.ShiftKeyMask) == NSEventModifierMask.ShiftKeyMask)
+			if ((theEvent.ModifierFlags & NSEventModifierMask.AlphaShiftKeyMask) == NSEventModifierMask.AlphaShiftKeyMask ||
+			    (theEvent.ModifierFlags & NSEventModifierMask.ShiftKeyMask) == NSEventModifierMask.ShiftKeyMask)
 				NSAnimationContext.EndGrouping();
 		}
 		
-		private CALayer makeCompositionLayer()
+		private CALayer MakeCompositionLayer ()
 		{
 			QCCompositionRepository repo = QCCompositionRepository.SharedCompositionRepository;
 			
-			QCComposition composition = repo.GetComposition("/moving shapes");
-			QCCompositionLayer compLayer = QCCompositionLayer.Create(composition);
+			QCComposition composition = repo.GetComposition ("/moving shapes");
+			QCCompositionLayer compLayer = QCCompositionLayer.Create (composition);
 			//CGColor cgColor = new CGColor(0.25f, 0.675f, 0.1f, 1.0f);
-			NSColor nsColor = NSColor.FromCalibratedRgba(0.25f, 0.675f, 0.1f, 1.0f);
+			NSColor nsColor = NSColor.FromCalibratedRgba (0.25f, 0.675f, 0.1f, 1.0f);
 			
-			string path = String.Format("patch.{0}.value",QCComposition.InputPrimaryColorKey);
-			compLayer.SetValueForKeyPath(nsColor,(NSString)path);
+			string path = String.Format ("patch.{0}.value", QCComposition.InputPrimaryColorKey);
+			compLayer.SetValueForKeyPath (nsColor, (NSString)path);
 			
-			path = String.Format("patch.{0}.value",QCComposition.InputPaceKey);
-			compLayer.SetValueForKeyPath(NSNumber.FromFloat(5.0f),(NSString)path);
-			
-			
+			path = String.Format ("patch.{0}.value", QCComposition.InputPaceKey);
+			compLayer.SetValueForKeyPath (NSNumber.FromFloat (5.0f), (NSString) path);
+						
 			return compLayer;
 		}
 		
 		private NSImageView Mover()
 		{
-			if (mover == null) 
-			{
-				
-				mover = new NSImageView(Bounds);
-				mover.ImageScaling = NSImageScale.AxesIndependently;
-				mover.Image = NSImage.ImageNamed("photo.jpg");
-				
+			if (mover == null) {
 				float xInset = 0.25f * Bounds.Width;
 				float yInset = 0.25f * Bounds.Height;
 				
-				RectangleF moverFrame = Bounds.Inset(xInset,yInset);
-				PointF origin = new PointF(Bounds.GetMidX() - moverFrame.Width / 2.0f,
-				                           Bounds.GetMidY() - moverFrame.Height / 2.0f);
-				moverFrame.Location = origin;
+				RectangleF moverFrame = Bounds.Inset (xInset,yInset);
+				moverFrame.Location = new PointF (Bounds.GetMidX () - moverFrame.Width / 2.0f,
+				                            Bounds.GetMidY () - moverFrame.Height / 2.0f);
 				
-				mover.Frame = moverFrame;
-				mover.AlphaValue = 0.5f;
-				
+				mover = new NSImageView (Bounds) {
+					ImageScaling = NSImageScale.AxesIndependently,
+					Image = NSImage.ImageNamed("photo.jpg"),
+					Frame = moverFrame,
+					AlphaValue = 0.5f
+				};
 			}
 			return mover;
 		}
 		
+		// Constructors
+		
+		// Called when created from unmanaged code
+		public SharedContentView (IntPtr handle) : base(handle)
+		{
+		}
+
+		// Called when created directly from a XIB file
+		[Export("initWithCoder:")]
+		public SharedContentView (NSCoder coder) : base(coder)
+		{
+		}		
 	}
 }
 
