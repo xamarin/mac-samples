16cbd417afd7f7f1afb13b4a9d71c4e8d8e0ea8a
Craig Dunn <craig.dunn@conceptdevelopment.net>
2012-12-17 09:28:27 -0800

    [Hello_Mac] code for Getting Started article

---
+++ Hello_Mac/Hello_Mac/MainWindow.cs
@@ -0,0 +1,45 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using MonoMac.Foundation;
+using MonoMac.AppKit;
+
+namespace Test_Hello_Mac
+{
+	public partial class MainWindow : MonoMac.AppKit.NSWindow
+	{
+		protected int numberOfTimesClicked = 0;
+
+		#region Constructors
+		
+		// Called when created from unmanaged code
+		public MainWindow (IntPtr handle) : base (handle)
+		{
+			Initialize ();
+		}
+		
+		// Called when created directly from a XIB file
+		[Export ("initWithCoder:")]
+		public MainWindow (NSCoder coder) : base (coder)
+		{
+			Initialize ();
+		}
+		
+		// Shared initialization code
+		void Initialize ()
+		{
+		}
+
+		public override void AwakeFromNib ()
+		{
+			base.AwakeFromNib ();
+			ClickMeButton.Activated += (object sender, EventArgs e) => {
+				numberOfTimesClicked++;
+				OutputLabel.StringValue = "Clicked " + numberOfTimesClicked + " times.";
+			};
+		}
+		#endregion
+	}
+}
+
---
+++ Hello_Mac/Hello_Mac/MainWindow.designer.cs
@@ -0,0 +1,42 @@
+// WARNING
+//
+// This file has been generated automatically by MonoDevelop to store outlets and
+// actions made in the Xcode designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using MonoMac.Foundation;
+
+namespace Test_Hello_Mac
+{
+	[Register ("MainWindow")]
+	partial class MainWindow
+	{
+		[Outlet]
+		MonoMac.AppKit.NSTextField OutputLabel { get; set; }
+
+		[Outlet]
+		MonoMac.AppKit.NSButton ClickMeButton { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (OutputLabel != null) {
+				OutputLabel.Dispose ();
+				OutputLabel = null;
+			}
+
+			if (ClickMeButton != null) {
+				ClickMeButton.Dispose ();
+				ClickMeButton = null;
+			}
+		}
+	}
+
+	[Register ("MainWindowController")]
+	partial class MainWindowController
+	{
+		
+		void ReleaseDesignerOutlets ()
+		{
+		}
+	}
+}
