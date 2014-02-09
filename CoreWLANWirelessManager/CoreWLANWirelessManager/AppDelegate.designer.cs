87e7f37bc2999473275a75d752c02a2efe6589d0
olegoid <olegoid91@gmail.com>
2014-02-10 01:21:48 +0400

    [CoreWLANWirelessManager] Add new solution for CoreWLANWirelessManager sample

---
+++ CoreWLANWirelessManager/CoreWLANWirelessManager/AppDelegate.designer.cs
@@ -0,0 +1,10 @@
+﻿
+namespace CoreWLANWirelessManager
+{
+	// Should subclass MonoMac.AppKit.NSResponder
+	[MonoMac.Foundation.Register ("AppDelegate")]
+	public partial class AppDelegate
+	{
+	}
+}
+
