2fce61b28b3477256da57097fa673ad804e00ae5
olegoid <olegoid91@gmail.com>
2014-12-16 20:03:33 +0300

    [FilteredView] Migrate sample to use unified API: migration tool + expected api changes

--- CoreAnimationBook/FilteredView/MainMenu.xib.designer.cs
+++ CoreAnimationBook/FilteredView/MainMenu.xib.designer.cs
@@ -12,7 +12,7 @@ namespace FilteredView {
 	
 	
 	// Should subclass MonoMac.AppKit.NSResponder
-	[MonoMac.Foundation.Register("AppDelegate")]
+	[Foundation.Register("AppDelegate")]
 	public partial class AppDelegate {
 	}
 }
