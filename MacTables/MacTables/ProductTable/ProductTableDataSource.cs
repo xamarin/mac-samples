ddee96f20c354f3fa7126bad06fda0515002ddc2
Kevin Mullins <kevin.mullins@xamarin.com>
2015-09-03 10:12:27 -0500

    Xamarin.Mac: MacTables
    Half of the ProductTableDataSource.cs class had been mysteriously
    deleted. Fixed this issue.

--- MacTables/MacTables/ProductTable/ProductTableDataSource.cs
+++ MacTables/MacTables/ProductTable/ProductTableDataSource.cs
@@ -54,4 +54,16 @@ namespace MacTables
 			// Sort the data
 			if (oldDescriptors.Length > 0) {
 				// Update sort
-				Sort (oldDescriptors [0].Key, oldDescriptors [0].Ascendi
+				Sort (oldDescriptors [0].Key, oldDescriptors [0].Ascending);
+			} else {
+				// Grab current descriptors and update sort
+				NSSortDescriptor[] tbSort = tableView.SortDescriptors; 
+				Sort (tbSort[0].Key, tbSort[0].Ascending); 
+			}
+
+			// Refresh table
+			tableView.ReloadData ();
+		}
+		#endregion
+	}
+}
