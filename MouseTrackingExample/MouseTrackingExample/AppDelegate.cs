356a02edbaeba4b8c7e21ca342df84f80028da20
olegoid <olegoid91@gmail.com>
2015-10-29 15:34:42 -0300

    [MouseTrackingExample] Fix static analyzer issues: * remove unused using * remove empty methods * remove some redundant code

--- MouseTrackingExample/MouseTrackingExample/AppDelegate.cs
+++ MouseTrackingExample/MouseTrackingExample/AppDelegate.cs
@@ -1,23 +1,13 @@
 ﻿using AppKit;
 using Foundation;
-using ObjCRuntime;
 
 namespace MouseTrackingExample
 {
 	public partial class AppDelegate : NSApplicationDelegate
 	{
-		public AppDelegate ()
-		{
-		}
-
 		public override void DidFinishLaunching (NSNotification notification)
 		{
-			this.TrackingView.OnDragChange += (sender, e) => TheLabel.StringValue = e.Description;
-		}
-
-		public override void WillTerminate (NSNotification notification)
-		{
-			// Insert code here to tear down your application
+			TrackingView.OnDragChange += (sender, e) => TheLabel.StringValue = e.Description;
 		}
 	}
 }
--- MouseTrackingExample/MouseTrackingExample/MyTrackingView.cs
+++ MouseTrackingExample/MouseTrackingExample/MyTrackingView.cs
@@ -1,37 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Foundation;
+
 using AppKit;
 using CoreGraphics;
+using Foundation;
 
 namespace MouseTrackingExample
 {
-	public partial class MyTrackingView : AppKit.NSView
+	public partial class MyTrackingView : NSView
 	{
 		#region Constructors
 
 		// Called when created from unmanaged code
 		public MyTrackingView (IntPtr handle) : base (handle)
 		{
-			Initialize ();
 		}
 
 		// Called when created directly from a XIB file
 		[Export ("initWithCoder:")]
 		public MyTrackingView (NSCoder coder) : base (coder)
 		{
-			Initialize ();
-		}
-
-		// Shared initialization code
-		void Initialize ()
-		{
 		}
 
 		#endregion
 
-		NSTrackingArea trackingArea;	
+		NSTrackingArea trackingArea;
 		public event EventHandler<TrackingEventArgs> OnDragChange = delegate {};
 
 		public override void AwakeFromNib ()
@@ -41,7 +33,7 @@ namespace MouseTrackingExample
 			Layer.BackgroundColor = new CGColor (1, 0, 0);
 			trackingArea = new NSTrackingArea (Frame, NSTrackingAreaOptions.ActiveInKeyWindow | NSTrackingAreaOptions.MouseEnteredAndExited | NSTrackingAreaOptions.MouseMoved
 				, this, null);
-			this.AddTrackingArea (trackingArea);
+			AddTrackingArea (trackingArea);
 		}
 
 		public override void MouseEntered (NSEvent theEvent)
@@ -60,13 +52,13 @@ namespace MouseTrackingExample
 		{
 			base.MouseMoved (theEvent);
 			OnDragChange (this, new TrackingEventArgs ("Mouse Moved"));
-
 		}
 	}
 
 	public class TrackingEventArgs : EventArgs
 	{
 		public TrackingEventArgs (string description) { Description = description; }
+
 		public string Description { get; set; }
 	}
 }
