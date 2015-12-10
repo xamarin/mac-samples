8f5f08cb7f02245e510346ffbec96fa031eb5dde
Kevin Mullins <kevin.mullins@xamarin.com>
2015-12-10 12:43:29 -0600

    MacSourceLists
    Split out of the Outline view sample and updated to storyboard per
    GitHub defect #1017.

---
+++ MacSourceLists/MacSourceLists/ViewController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+
+namespace MacSourceLists
+{
+	public partial class ViewController : NSViewController
+	{
+		#region Computed Properties
+		public override NSObject RepresentedObject {
+			get {
+				return base.RepresentedObject;
+			}
+			set {
+				base.RepresentedObject = value;
+				// Update the view, if already loaded.
+			}
+		}
+		#endregion
+
+		#region Constructors
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+		#endregion
+
+		#region Override Methods
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Populate source list
+			SourceList.Initialize ();
+
+			var library = new SourceListItem ("Library");
+			library.AddItem ("Venues", "house.png", () => {
+				Console.WriteLine("Venue Selected");
+			});
+			library.AddItem ("Singers", "group.png");
+			library.AddItem ("Genre", "cards.png");
+			library.AddItem ("Publishers", "box.png");
+			library.AddItem ("Artist", "person.png");
+			library.AddItem ("Music", "album.png");
+			SourceList.AddItem (library);
+
+			// Add Rotation 
+			var rotation = new SourceListItem ("Rotation"); 
+			rotation.AddItem ("View Rotation", "redo.png");
+			SourceList.AddItem (rotation);
+
+			// Add Kiosks
+			var kiosks = new SourceListItem ("Kiosks");
+			kiosks.AddItem ("Sign-in Station 1", "imac");
+			kiosks.AddItem ("Sign-in Station 2", "ipad");
+			SourceList.AddItem (kiosks);
+
+			// Display side list
+			SourceList.ReloadData ();
+			SourceList.ExpandItem (null, true);
+		}
+		#endregion
+	}
+}
---
+++ MacSourceLists/Readme.md
@@ -0,0 +1,8 @@
+MacSourceLists
+===========
+
+Source code for the [Source Lists](/guides/mac/user-interface/working-with-sourcelists/) documentation on [Xamarin Developer Center](http://docs.xamarin.com)
+
+Uses [Xamarin.Mac](http://xamarin.com).
+
+This sample covers working with Source Lists in a Xamarin.Mac application. It covers creating and maintaining Source Lists in Xcode and Interface builder, how to expose the Source Lists to code using Outlets and Actions, populating Source Lists Items and finally responding to Source Lists Items in C# code.
