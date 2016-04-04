422b388d9ab518bee27208e95954e3a4396ea80a
Kevin Mullins <kevin.mullins@xamarin.com>
2016-04-04 10:17:26 -0500

    MacTables Updated for Delete
    Per GitHub issue #1357, added the ability to delete a row from the
    table view.

--- MacTables/MacTables/ProductTable/ProductTableDelegate.cs
+++ MacTables/MacTables/ProductTable/ProductTableDelegate.cs
@@ -15,12 +15,46 @@ namespace MacTables
 
 		#region Private Variables
 		private ProductTableDataSource DataSource;
+		private ViewController Controller;
 		#endregion
 
 		#region Constructors
-		public ProductTableDelegate (ProductTableDataSource datasource)
+		public ProductTableDelegate (ViewController controller, ProductTableDataSource datasource)
 		{
+			this.Controller = controller;
 			this.DataSource = datasource;
+		}
+		#endregion
+
+		#region Private Methods
+		private void ConfigureTextField (NSTableCellView view, nint row)
+		{
+			// Add to view
+			view.TextField.AutoresizingMask = NSViewResizingMask.WidthSizable;
+			view.AddSubview (view.TextField);
+
+			// Configure
+			view.TextField.BackgroundColor = NSColor.Clear;
+			view.TextField.Bordered = false;
+			view.TextField.Selectable = false;
+			view.TextField.Editable = true;
+
+			// Wireup events
+			view.TextField.EditingEnded += (sender, e) => {
+
+				// Take action based on type
+				switch (view.Identifier) {
+				case "Product":
+					DataSource.Products [(int)view.TextField.Tag].Title = view.TextField.StringValue;
+					break;
+				case "Details":
+					DataSource.Products [(int)view.TextField.Tag].Description = view.TextField.StringValue;
+					break;
+				}
+			};
+
+			// Tag view
+			view.TextField.Tag = row;
 		}
 		#endregion
 
@@ -34,46 +68,78 @@ namespace MacTables
 			NSTableCellView view = (NSTableCellView)tableView.MakeView (tableColumn.Title, this);
 			if (view == null) {
 				view = new NSTableCellView ();
-				if (tableColumn.Title == "Product") {
+
+				// Configure the view
+				view.Identifier = tableColumn.Title;
+
+				// Take action based on title
+				switch (tableColumn.Title) {
+				case "Product":
 					view.ImageView = new NSImageView (new CGRect (0, 0, 16, 16));
 					view.AddSubview (view.ImageView);
 					view.TextField = new NSTextField (new CGRect (20, 0, 400, 16));
-				} else {
+					ConfigureTextField (view, row);
+					break;
+				case "Details":
 					view.TextField = new NSTextField (new CGRect (0, 0, 400, 16));
+					ConfigureTextField (view, row);
+					break;
+				case "Action":
+					// Create new button
+					var button = new NSButton (new CGRect (0, 0, 81, 16));
+					button.SetButtonType (NSButtonType.MomentaryPushIn);
+					button.Title = "Delete";
+					button.Tag = row;
+
+					// Wireup events
+					button.Activated += (sender, e) => {
+						// Get button and product
+						var btn = sender as NSButton;
+						var product = DataSource.Products [(int)btn.Tag];
+
+						// Configure alert
+						var alert = new NSAlert () {
+							AlertStyle = NSAlertStyle.Informational,
+							InformativeText = $"Are you sure you want to delete {product.Title}? This operation cannot be undone.",
+							MessageText = $"Delete {product.Title}?",
+						};
+						alert.AddButton ("Cancel");
+						alert.AddButton ("Delete");
+						alert.BeginSheetForResponse (Controller.View.Window, (result) => {
+							// Should we delete the requested row?
+							if (result == 1001) {
+								// Remove the given row from the dataset
+								DataSource.Products.RemoveAt((int)btn.Tag);
+								Controller.ReloadTable ();
+							}
+						});
+					};
+
+					// Add to view
+					view.AddSubview (button);
+					break;
 				}
-				view.TextField.AutoresizingMask = NSViewResizingMask.WidthSizable;
-				view.AddSubview (view.TextField);
-				view.Identifier = tableColumn.Title;
-				view.TextField.BackgroundColor = NSColor.Clear;
-				view.TextField.Bordered = false;
-				view.TextField.Selectable = false;
-				view.TextField.Editable = true;
 
-				view.TextField.EditingEnded += (sender, e) => {
-
-					// Take action based on type
-					switch(view.Identifier) {
-					case "Product":
-						DataSource.Products [(int)view.TextField.Tag].Title = view.TextField.StringValue;
-						break;
-					case "Details":
-						DataSource.Products [(int)view.TextField.Tag].Description = view.TextField.StringValue;
-						break; 
-					}
-				};
 			}
-
-			// Tag view
-			view.TextField.Tag = row;
 
 			// Setup view based on the column selected
 			switch (tableColumn.Title) {
 			case "Product":
 				view.ImageView.Image = NSImage.ImageNamed ("tag.png");
 				view.TextField.StringValue = DataSource.Products [(int)row].Title;
+				view.TextField.Tag = row;
 				break;
 			case "Details":
 				view.TextField.StringValue = DataSource.Products [(int)row].Description;
+				view.TextField.Tag = row;
+				break;
+			case "Action":
+				foreach (NSView subview in view.Subviews) {
+					var btn = subview as NSButton;
+					if (btn != null) {
+						btn.Tag = row;
+					}
+				}
 				break;
 			}
 
--- MacTables/MacTables/ViewController.cs
+++ MacTables/MacTables/ViewController.cs
@@ -25,6 +25,13 @@ namespace MacTables
 		}
 		#endregion
 
+		#region Public Methods
+		public void ReloadTable ()
+		{
+			ProductTable.ReloadData ();
+		}
+		#endregion
+
 		#region Override Methods
 		public override void ViewDidLoad ()
 		{
@@ -35,11 +42,19 @@ namespace MacTables
 			DataSource.Products.Add (new Product ("Xamarin.iOS", "Allows you to develop native iOS Applications in C#"));
 			DataSource.Products.Add (new Product ("Xamarin.Android", "Allows you to develop native Android Applications in C#"));
 			DataSource.Products.Add (new Product ("Xamarin.Mac", "Allows you to develop Mac native Applications in C#"));
+			DataSource.Products.Add (new Product ("Xamarin.tvOS", "Allows you to develop Apple TV native Applications in C#"));
+			DataSource.Products.Add (new Product ("Xamarin Studio Community", "A free, full-featured IDE for Mac users to create Android and iOS apps using Xamarin."));
+			DataSource.Products.Add (new Product ("Visual Studio Community", "A free, full-featured and extensible IDE for Windows users to create Android and iOS apps with Xamarin, as well as Windows apps, web apps, and cloud services."));
+			DataSource.Products.Add (new Product ("Visual Studio Professional", "Professional developer tools and services for individual developers or small teams."));
+			DataSource.Products.Add (new Product ("Visual Studio Enterprise", "End-to-end solution for teams of any size with demanding quality and scale needs"));
+			DataSource.Products.Add (new Product ("Xamarin Test Cloud", "Automatically test your app on thousands of mobile devices."));
+			DataSource.Products.Add (new Product ("HockyApp", "Bring Mobile DevOps to your apps and reliability to your users."));
+			DataSource.Products.Add (new Product ("Xamarin University", "Take your mobile strategy and apps to the next level."));
 			DataSource.Sort ("Title", true);
 
 			// Populate the Product Table
 			ProductTable.DataSource = DataSource;
-			ProductTable.Delegate = new ProductTableDelegate (DataSource);
+			ProductTable.Delegate = new ProductTableDelegate (this, DataSource);
 
 			// Auto select the first row
 			ProductTable.SelectRow (0, false);
