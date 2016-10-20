4e105532a8dfc685e5b42290c087658c452e7538
Chris Hamons <chris.hamons@xamarin.com>
2016-10-20 10:48:41 -0500

    Port ImageProtocolSample to XM from XI

---
+++ ImageProtocolSample/ImageProtocolSample/ViewController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+using AppKit;
+using Foundation;
+using WebKit;
+
+namespace ImageProtocolSample
+{
+	public partial class ViewController : NSViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			// Register our custom url protocol
+			NSUrlProtocol.RegisterClass (new ObjCRuntime.Class (typeof (ImageProtocol)));
+
+			WebView web = new WebView ()
+			{
+				AutoresizingMask = NSViewResizingMask.HeightSizable | NSViewResizingMask.WidthSizable
+			};
+
+			web.Frame = View.Bounds;
+			View.AutoresizesSubviews = true;
+			View.AddSubview (web);
+
+			web.MainFrame.LoadRequest (NSUrlRequest.FromUrl (NSUrl.FromFilename (NSBundle.MainBundle.PathForResource ("test", "html"))));
+		}
+	}
+}
