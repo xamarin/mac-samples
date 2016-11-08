c1c85460d3047d4971d3b257af07c2092f9a7aa6
Craig Dunn <craig.dunn@conceptdevelopment.net>
2016-11-08 08:56:52 -0800

    [AzureTodo] update to latest Azure package

--- AzureTodoMac/AzureTodoMac/Constants.cs
+++ AzureTodoMac/AzureTodoMac/Constants.cs
@@ -7,7 +7,6 @@
 		// Before you can successfully run this example application, you'll need to replace these
 		// strings with ones that match your developer profile with Azure
 		public static string ApplicationURL = string.Empty; // Example: @"https://company-name.azurewebsites.net"
-		public static string ApplicationKey = string.Empty; // Example: @"CBnos...VYM69";
 	}
 }
 
