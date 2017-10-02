4657da321c92ec5633c297675d094879cbcb6d52
Chris Hamons <chris.hamons@xamarin.com>
2017-10-02 07:44:05 -0500

    A simple NSScrollView example

---
+++ NSScrollExample/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace NSScrollExample
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
