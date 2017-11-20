a355b5d6faea062df04915040581423b2a52cebb
amitgosavi1 <amit.gosavi1@globallogic.com>
2017-11-20 15:07:31 +0530

    Formatting updated.

--- MacCopyPaste/MacCopyPaste/Classes/ImageInfo.cs
+++ MacCopyPaste/MacCopyPaste/Classes/ImageInfo.cs
@@ -34,7 +34,7 @@ namespace MacCopyPaste
 			var type = decoder.DecodeObject("imageType") as NSString;
 
 			// Save data
-            		if (name!= null && type != null)
+            		if (name != null && type != null)
             		{
                 		Name = name.ToString();
                 		ImageType = type.ToString();
@@ -46,11 +46,11 @@ namespace MacCopyPaste
 		[Export ("encodeWithCoder:")]
 		public void EncodeTo (NSCoder encoder)
 		{
-            		if (Name != null && ImageType != null)
-            		{
-                		encoder.Encode(new NSString(Name), "name");
-                		encoder.Encode(new NSString(ImageType), "imageType");
-            		}
+			if (Name != null && ImageType != null)
+			{
+				encoder.Encode(new NSString(Name), "name");
+				encoder.Encode(new NSString(ImageType), "imageType");
+			}
 		}
 
 		[Export ("writableTypesForPasteboard:")]
