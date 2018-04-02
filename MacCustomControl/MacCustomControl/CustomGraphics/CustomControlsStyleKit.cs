31fbd205f9f5def3d158b9e026c327dc85fe1eb2
Mykyta Bondarenko <32954580+mykyta-bondarenko-gl@users.noreply.github.com>
2018-04-02 18:21:16 +0300

    removed `System.Drawing` usings; "MacCustomControl" project (#77)

--- MacCustomControl/MacCustomControl/CustomGraphics/CustomControlsStyleKit.cs
+++ MacCustomControl/MacCustomControl/CustomGraphics/CustomControlsStyleKit.cs
@@ -10,7 +10,6 @@
 
 
 using System;
-using System.Drawing;
 using Foundation;
 using UIKit;
 using CoreGraphics;
--- MacCustomControl/MacCustomControl/UIKit/UIImage.cs
+++ MacCustomControl/MacCustomControl/UIKit/UIImage.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Drawing;
 using Foundation;
 using AppKit;
 using CoreGraphics;
