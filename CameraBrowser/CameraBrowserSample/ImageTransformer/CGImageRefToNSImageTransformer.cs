b007bcd6388324a7479cd16d2d2ca2ea81cf1140
Haritha Mohan <harithamohan@microsoft.com>
2023-01-24 22:15:01 -0800

    address review, nullability, identation

--- CameraBrowser/CameraBrowserSample/ImageTransformer/CGImageRefToNSImageTransformer.cs
+++ CameraBrowser/CameraBrowserSample/ImageTransformer/CGImageRefToNSImageTransformer.cs
@@ -2,26 +2,27 @@
 using ObjCRuntime;
 using AppKit;
 using CoreGraphics;
+#nullable enable
 
 namespace CameraBrowserSample
 {
-    [Register("CGImageRefToNSImageTransformer")]
-    public class CGImageRefToNSImageTransformer: NSValueTransformer
+	[Register("CGImageRefToNSImageTransformer")]
+	public class CGImageRefToNSImageTransformer: NSValueTransformer
 	{
-        [Export("transformedValueClass")]
-        public static new Class TransformedValueClass => new Class(typeof(NSImage));
+		[Export("transformedValueClass")]
+		public static new Class TransformedValueClass => new Class(typeof(NSImage));
 
-        [Export("allowsReverseTransformation")]
-        public static new bool AllowsReverseTransformation => false;
+		[Export("allowsReverseTransformation")]
+		public static new bool AllowsReverseTransformation => false;
 
-        public override NSObject TransformedValue(NSObject value)
-        {
-            if (value == null)
-                return null;
+		public override NSObject? TransformedValue(NSObject? value)
+		{
+			if (value is null)
+				return null;
 
-            var img = Runtime.GetINativeObject<CGImage>(value.Handle, false);
+			CGImage img = Runtime.GetINativeObject<CGImage>(value.Handle, false)!;
             return new NSImage(img, CGSize.Empty);
-        }
-    }
+		}
+	}
 }
 
